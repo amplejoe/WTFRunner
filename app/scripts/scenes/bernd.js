61820062d90a1ae220b3ac115178cdc07f1cd7a1
Bernd Muenzer <bermuenz@aau.at>
2018-04-28 15:05:16 +0200

    health bar color

--- app/scripts/scenes/bernd.js
+++ app/scripts/scenes/bernd.js
@@ -57,23 +57,15 @@ export default class Bernd extends Phaser.Scene {
     });
     help.setScrollFactor(0);
 
-    // this.playerHealthMeter = this.game.add.plugin(Phaser.Plugin.HealthMeter);
-    // this.playerHealthMeter.bar(
-    //     this.player,
-    //     {x: 20, y: 100, width: 100, height: 20}
-    // );
 
 
     this.input.keyboard.on('keydown_A', (event) => {
-      this.hurt(1);
+        this.hurt(1);
     });
 
     this.input.keyboard.on('keydown_S', (event) => {
-      this.heal(1);
+        this.heal(1);
     });
-
-
-    this.healthBar
 
     this.healthBarBack = this.add.graphics({ fillStyle: { color: 0x0aaf2e } });
     this.healthBar = this.add.graphics({ fillStyle: { color: 0x7df433 } });
@@ -99,11 +91,11 @@ export default class Bernd extends Phaser.Scene {
   }
 
   hurt(amount) {
-      this.health -= amount;
-      if (this.health < 0) {
+      this.health = Math.max(0, this.health - amount);
+      this.updateHealthBar();
+      if (this.health <= 0) {
           this.gameOver();
       }
-      this.updateHealthBar();
   }
 
   gameOver() {
@@ -114,8 +106,51 @@ export default class Bernd extends Phaser.Scene {
     var height = 90 * this.health/this.maxHealth;
     var rect = new Phaser.Geom.Rectangle(this.cameras.main.width - 45, this.cameras.main.height - height - 25, 20, height);
     this.healthBar.clear();
+    this.healthBar.fillStyle(this.rgbToHex(this.HSVtoRGB(this.health/this.maxHealth*0.38, 1, 1)));
     this.healthBar.fillRectShape(rect);
   }
+
+// 120 - 0  ~
+
+  /* accepts parameters
+ * h  Object = {h:x, s:y, v:z}
+ * OR
+ * h, s, v
+*/
+HSVtoRGB(h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    if (arguments.length === 1) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.round(r * 255),
+        g: Math.round(g * 255),
+        b: Math.round(b * 255)
+    };
+
+}
+
+componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+rgbToHex(rgb) {
+    return "0x" + this.componentToHex(rgb.r) + this.componentToHex(rgb.g) + this.componentToHex(rgb.b);
+}
 
   /**
    *  Handles updates to game logic, physics and game objects.
