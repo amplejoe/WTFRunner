860f9ab911372d280e6d7496d31a7e0c4e534e7b
amplejoe <amplejoe@gmail.com>
2018-04-29 16:01:22 +0200

    renamed anims to fix warnings

--- app/scripts/scenes/title.js
+++ app/scripts/scenes/title.js
@@ -21,7 +21,7 @@ export default class Title extends Phaser.Scene {
 
     var introScreen = this.add.image(0, 0, 'intro-screen').setOrigin(0);
 
-    this.dancingChar = this.add.sprite(introScreen.width/4, introScreen.height/2, 'dance');
+    this.dancingChar = this.add.sprite(introScreen.width/4, introScreen.height/2, 'dance2');
     this.powerUpCan = this.add.sprite(introScreen.width/3, introScreen.height/2, 'powerUp');
 
     var startbutton = this.add.sprite(introScreen.width * (2/3), introScreen.height * (2.5/4), 'start').setOrigin(0);
@@ -31,14 +31,14 @@ export default class Title extends Phaser.Scene {
     startbutton.on('pointerup', () => this.scene.start('Level'));
 
     this.anims.create({
-      key: 'spin_can',
+      key: 'spin_can2',
       frames: this.anims.generateFrameNumbers('powerUp', { start: 0, end: 3 }),
       frameRate: 10,
       repeat: -1
     });
 
     this.anims.create({
-      key: 'dance',
+      key: 'dance2',
       frames: this.anims.generateFrameNumbers('player', {frames: [1,2,3,2,1,6,5,4,5,6]}),
       frameRate: 10,
       repeat: -1
@@ -52,10 +52,10 @@ export default class Title extends Phaser.Scene {
     });
     startbutton.anims.play('startbutton',true);
 
-    this.powerUpCan.anims.play('spin_can',true);
+    this.powerUpCan.anims.play('spin_can2',true);
     this.powerUpCan.rotation = 0.2;
 
-    this.dancingChar.anims.play('dance',true);
+    this.dancingChar.anims.play('dance2',true);
     this.dancingChar.rotation = 0.5;
 
     this.music = this.sound.add('startMusic');
